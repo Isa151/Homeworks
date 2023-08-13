37a107ef99ed34a13323841ee8e64ab5aa88dc56
Islom <141430075+Isa151@users.noreply.github.com>
2023-08-13 09:20:45 +0500

    Add files via upload

---
+++ js homework/main.js
@@ -0,0 +1,33 @@
+ let lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nihil dolorum ab, quis doloribus soluta aliquam veritatis possimus consequatur voluptatem. Sapiente, fuga perferendis. Obcaecati inventore eligendi blanditiis nemo est quos?'
+
+
+   
+     let i = ''
+    
+     console.log(lorem.slice(5, 7).trim())
+
+     let s = ''
+
+      console.log(lorem.slice(8, 9))
+
+      let l = ''
+
+      console.log(lorem.slice(0, 1))
+
+      let o = ''
+
+      console.log(lorem.slice(1, 2))
+
+      let m = ''
+
+      console.log(lorem.slice(4, 5))
+
+      
+      let name = ''
+
+       console.log(`i`+ `s`+ `l`+ `o`+ `m`)
+
+
+    
+   
+ 
