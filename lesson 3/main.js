29e49ae351c6d1dc938c4334f2a6ff3f86c5775e
Islom <141430075+Isa151@users.noreply.github.com>
2023-08-14 19:37:10 +0500

    Add files via upload

---
+++ lesson 3/main.js
@@ -0,0 +1,30 @@
+
+
+let str = "HTML JavaScript Python";
+console.log(str.replaceAll(' ',  '-'));
+
+let str1 = 'string not starting with capital'
+str1 = str1.replace(
+    str1[0], str1[0].toUpperCase()
+)
+
+console.log(str1)
+
+
+let str2 = 'КаЖдЫй ОхОтНиК жЕлАеТ зНаТь'
+ 
+console.log(str2.toLowerCase().replace('к', 'К'))
+
+let string = ''
+let StRiNg = ''
+console.log(string===StRiNg)
+
+let ABCDe = ''
+let AbcdW = ''
+
+console.log('ABCDe'==='AbcdW' )
+
+
+
+
+
