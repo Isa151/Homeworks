2d3daa29d4145efbdea978a3dc8bb828c222c7e7
Islom <141430075+Isa151@users.noreply.github.com>
2023-09-06 21:51:42 +0500

    Add files via upload

---
+++ js homework 6/main.js
@@ -0,0 +1,58 @@
+
+
+let str = 'aaa@bbb@ccc'
+
+console.log(str.replaceAll('@' , '!'));
+   
+
+let str2 = "HELLO WORLD";
+let words = str2.split(' ');
+let result = words.map(word => word.charAt(0) + word.slice(1).toLowerCase()).join(' ');
+console.log(result);
+
+
+let html = 'Hello, it is HTML'
+
+let js = 'Hello it is not JS'
+
+console.log(
+    html = js
+);
+
+let a = 'alex'
+a = a.replace(
+    a[0], a[0].toUpperCase()
+)
+
+console.log(a)
+
+
+let alex = 'Lex Luter has a big suit'
+
+let A = ''
+
+console.log(
+    alex.slice(11,12)
+);
+
+let L = ''
+
+console.log(
+    alex.slice(0,1)
+);
+
+let e = ''
+
+console.log(
+    alex.slice(1,2)
+);
+
+let x = ''
+
+console.log(
+    alex.slice(2,3)
+);
+
+console.log(
+   (a +L +e +x)
+);
