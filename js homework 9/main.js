e2a98f774dd9494c18df7a04a43c4415a17e4cc8
Islom <141430075+Isa151@users.noreply.github.com>
2023-09-13 22:02:28 +0500

    Add files via upload

---
+++ js homework 9/main.js
@@ -0,0 +1,27 @@
+
+
+// let question = confirm("Ваше первя буква в имени начинается с А?")
+
+// if(question === true){
+//     alert('Вход разрешён!')
+// }else{
+//     alert('Вход воспрещён!')
+// }
+
+
+// let number = prompt('Введите любyю цыфру')
+
+// if(number % 2 === 0){
+//     alert("Чётное")
+// }else{
+//     alert("Не чётное")
+// }
+
+
+// let name = prompt("Введите своё имя")
+
+// if(name.length % 2 === 0){
+//     alert('Welcome')
+// }else{
+//     alert("Go home")
+// }
