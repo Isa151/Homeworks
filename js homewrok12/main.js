efb99b9d3105c06cc10fea89655bf6969ab8e3db
Islom <141430075+Isa151@users.noreply.github.com>
2023-09-21 15:22:55 +0500

    Delete js homewrok12 directory

--- js homewrok12/main.js
+++
@@ -1,8 +0,0 @@
-
-
-let arr = ['islom', 'muhammadali', 'baxtzod', 'usmon', 'xayot', 'artyom']
-
-let name = prompt("Which student do want to remove?")
-arr.splice(arr.indexOf(name), 1)
-
-console.log(arr);
