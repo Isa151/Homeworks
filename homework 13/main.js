d285ea06fc0c29e4ee81c13bb843f826c0e94a08
Islom <141430075+Isa151@users.noreply.github.com>
2023-09-22 21:50:20 +0500

    Add files via upload

---
+++ homework 13/main.js
@@ -0,0 +1,25 @@
+
+
+let carPrices = [30000, 2000, 11000, 33000, 2000, 100000, 57000, 14000, 18000, 8000];
+let budget = Math.floor(Math.random() * (120000 - 2000 + 1) + 2000);
+
+let filtered = carPrices.filter(price => price <= budget);
+if (filtered.length === 0) {
+  alert(`К сожалению, для бюджета ${budget} нет доступных машин.`);
+} else {
+  let carList = "Для бюджета " + budget + " доступны машины со следующими ценами:\n";
+  filtered.forEach(price => {
+    carList += "- " + price + "\n";
+  });
+  alert(carList);
+}
+
+
+
+
+
+const names = ['baxtzod', 'xamza', 'islom', 'abdumanon', 'xojik', 'aziz'];
+
+const longestName = names.filter(name => name.length === names.reduce((acc, curr) => acc > curr.length ? acc : curr.length, 0))[0];
+
+console.log(`Самое длинное имя в массиве: ${longestName}`);
